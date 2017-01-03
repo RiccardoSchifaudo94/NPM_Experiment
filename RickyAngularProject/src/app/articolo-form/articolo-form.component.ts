f96e24a968da28aea1126c400535f0bddb1ddf0f
riccardo schifaudo <riccardo schifaudo>
2017-01-03 23:53:27 +0100

    creazione funzione mixin sass per generazione dinamica dei bottoni

--- RickyAngularProject/src/app/articolo-form/articolo-form.component.ts
+++ RickyAngularProject/src/app/articolo-form/articolo-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Articolo } from '../articolo/articolo';
 
 @Component({
-  selector: 'app-articolo-form',
+  selector: 'articolo-form',
   templateUrl: './articolo-form.component.html',
   styleUrls: ['./articolo-form.component.scss']
 })
